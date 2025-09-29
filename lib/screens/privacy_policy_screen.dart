adb81b0e7b30795fcda18ab1ab11b124ba0063f8
SergeyFierce <102462393+SergeyFierce@users.noreply.github.com>
2025-09-29 22:49:04 +0300

    Add webview screens for privacy policy and user agreement

--- lib/screens/privacy_policy_screen.dart
+++ lib/screens/privacy_policy_screen.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class PrivacyPolicyScreen extends StatelessWidget {
+class PrivacyPolicyScreen extends StatefulWidget {
   const PrivacyPolicyScreen({super.key});
+
+  @override
+  State<PrivacyPolicyScreen> createState() => _PrivacyPolicyScreenState();
+}
+
+class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
+  late final WebViewController _controller;
+  double _progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (progress) {
+            setState(() {
+              _progress = progress / 100;
+            });
+          },
+        ),
+      )
+      ..loadRequest(
+        Uri.parse('https://privacytouchnotebook.netlify.app/'),
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Политика конфиденциальности')),
-      body: const Center(
-        child: Padding(
-          padding: EdgeInsets.all(24),
-          child: Text(
-            'Текст политики конфиденциальности появится здесь позже.',
-            textAlign: TextAlign.center,
+      body: Column(
+        children: [
+          if (_progress < 1)
+            LinearProgressIndicator(
+              value: _progress,
+            ),
+          Expanded(
+            child: WebViewWidget(controller: _controller),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/screens/user_agreement_screen.dart
+++ lib/screens/user_agreement_screen.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class UserAgreementScreen extends StatelessWidget {
+class UserAgreementScreen extends StatefulWidget {
   const UserAgreementScreen({super.key});
+
+  @override
+  State<UserAgreementScreen> createState() => _UserAgreementScreenState();
+}
+
+class _UserAgreementScreenState extends State<UserAgreementScreen> {
+  late final WebViewController _controller;
+  double _progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (progress) {
+            setState(() {
+              _progress = progress / 100;
+            });
+          },
+        ),
+      )
+      ..loadRequest(
+        Uri.parse('https://eulatouchnotebook.netlify.app/'),
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Пользовательское соглашение')),
-      body: const Center(
-        child: Padding(
-          padding: EdgeInsets.all(24),
-          child: Text(
-            'Раздел пользовательского соглашения находится в разработке.',
-            textAlign: TextAlign.center,
+      body: Column(
+        children: [
+          if (_progress < 1)
+            LinearProgressIndicator(
+              value: _progress,
+            ),
+          Expanded(
+            child: WebViewWidget(controller: _controller),
           ),
-        ),
+        ],
       ),
     );
   }
